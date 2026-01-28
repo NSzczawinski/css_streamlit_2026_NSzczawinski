68e437622a716533e7b58fe1e7f31871a0c4c738
NSzczawinski <100418799+NSzczawinski@users.noreply.github.com>
2026-01-28 15:30:22 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import NN
 
 # Set page title
 st.set_page_config(page_title = "Researcher Profile and Distribution Reconstructor", layout = "wide")
@@ -34,12 +35,40 @@ elif menu == "Current Work":
 
 elif menu == "Distribution Reconstructor":
     st.title("Distribution Reconstructor")
+    
+    random_seed= st.checkbox("Use a random seed?:", value = False)
+    samples_count = st.number_input("How many samples do you want to reconstruct?:", value = 5000)
+    frequencies_count = st.number_input("How many samples of the characteristic function do you want to take?:", value = 100)
+    dimension = st.number_input("Dimension of the distribution:", value = 2)
+    
+    topology_options = [5000, 2500, 1000, 500, 300, 100, 50, 25, 10, 2]
+    topology_list = st.multiselect("Select neuron layers in order:", topology_options, default = [300, 50, 2])
+    st.write("Your list:", activation_list)
+    
+    activation_options = ["sigmoid", "ReLU", "purelin", "sin", "tanh"]
+    activation_list = st.multiselect("Select activation functions in order:", activation_options, default = ["sin", "sin", "purelin"])
+    st.write("Your list:", activation_list)
+
+    batchsize = st.text_input("Select batchsize:", value = "fullbatch")
+    resample_factor = st.number_input("After how many iterations do you want to resample the noise?:", value = 100)
+    lr = st.number_input("Learn rate:", value = 0.001)
+    passes = st.number_input("How many iterations should the network complete?:", value = 10000)
+
+    loss_options = ["SEL", "SEL+"]
+    loss_list = st.multiselect("Select the loss type:", loss_options, default = ["SEL+"])
+    st.write("Your loss is set to:", loss_list)
+
+if st.button("Run NN"):
+    # Call the NN function
+    # result = NN(user_input)
+    # st.success(f"The NN output is: {result}")
         
 elif menu == "Contact":
     st.header("Contact Information")
     email = "jane.doe@example.com"
 
     st.write(f"You can reach me at {email}.")
+
 
 
 
