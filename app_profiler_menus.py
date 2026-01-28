d3dba703d5f459b287b624ba88ead5ad01f875fd
NSzczawinski <100418799+NSzczawinski@users.noreply.github.com>
2026-01-28 15:54:56 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -44,15 +44,17 @@ elif menu == "Distribution Reconstructor":
     topology_list = st.multiselect("Select neuron layers in order (must end with 2):", topology_options, default = [300, 50, 2])
     st.write("Your list:", topology_list)
 
-    selected_activations = st.session_state.get("selected", [])
-    def update_selection():
-        st.session_state.selected = selected
     activation_options = ["sigmoid", "ReLU", "purelin", "sin", "tanh"]
-    max_choices = 3
-    activation_list = st.multiselect("Select activation functions in order:", activation_options, default = selected_activations, key = "multi_select", help = f"Pick up to {max_choices} items")
+    activation_list = st.multiselect("Select activation functions in order:", activation_options, default = ["sin", "sin", "purelin"])
     st.write("Your list:", activation_list)
+    
+    selected_loss = st.session_state.get("selected_loss", [])
+    def update_selection():
+        st.session_state.selected = selected_loss
+    activation_options = ["SEL", "SEL+"]
+    max_choices = 1
     if len(activation_list) > max_choices:
-        st.warning(f"You can select at most {max_choices} items")
+        st.warning(f"You can select at most {max_choices} loss function")
 
     batchsize = st.text_input("Select batchsize:", value = "fullbatch")
     resample_factor = st.number_input("After how many iterations do you want to resample the noise?:", value = 100)
@@ -75,6 +77,7 @@ elif menu == "Contact":
     email = "jane.doe@example.com"
 
     st.write(f"You can reach me at {email}.")
+
 
 
 
