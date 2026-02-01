8ae32566c1dc12804f2a55436825a34c3801195a
NSzczawinski <100418799+NSzczawinski@users.noreply.github.com>
2026-02-02 00:39:22 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -98,7 +98,7 @@ if menu == "Researcher Profile":
 elif menu == "Current Work":
     # -------------------------------------------------------------------------------------------------------------
     st.title("Current Work")
-    with open("streamlit_eg_1\MST Hons Research Project Nicholas Szczawinski 17s0395.pdf", "rb") as file:
+    with open("streamlit_eg_1/MST Hons Research Project Nicholas Szczawinski 17s0395.pdf", "rb") as file:
     base64_pdf = base64.b64encode(file.read()).decode("utf-8")
 
     st.markdown(
@@ -164,6 +164,7 @@ elif menu == "Distribution Reconstructor Demo":
     
             time.sleep(0.4)
     # -------------------------------------------------------------------------------------------------------------
+
 
 
 
